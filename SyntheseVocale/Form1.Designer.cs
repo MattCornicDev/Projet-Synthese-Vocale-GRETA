fb93b7b1ba442e73420f07bd5be75e61717ed684
MattCornicDev <glgharib@gmail.com>
2021-03-12 15:50:02 +0100

    Projet Synthese Vocale Premier dépot

---
+++ SyntheseVocale/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace SyntheseVocale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxReco = new System.Windows.Forms.RichTextBox();
+            this.labelMotReconnu = new System.Windows.Forms.Label();
+            this.buttonParler = new System.Windows.Forms.Button();
+            this.labelImageAssociee = new System.Windows.Forms.Label();
+            this.pictureBoxAssociee = new System.Windows.Forms.PictureBox();
+            this.buttonQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssociee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxReco
+            // 
+            this.richTextBoxReco.Location = new System.Drawing.Point(161, 112);
+            this.richTextBoxReco.Name = "richTextBoxReco";
+            this.richTextBoxReco.Size = new System.Drawing.Size(392, 432);
+            this.richTextBoxReco.TabIndex = 0;
+            this.richTextBoxReco.Text = "";
+            // 
+            // labelMotReconnu
+            // 
+            this.labelMotReconnu.AutoSize = true;
+            this.labelMotReconnu.Location = new System.Drawing.Point(275, 40);
+            this.labelMotReconnu.Name = "labelMotReconnu";
+            this.labelMotReconnu.Size = new System.Drawing.Size(132, 25);
+            this.labelMotReconnu.TabIndex = 1;
+            this.labelMotReconnu.Text = "Mot reconnu";
+            // 
+            // buttonParler
+            // 
+            this.buttonParler.Location = new System.Drawing.Point(265, 585);
+            this.buttonParler.Name = "buttonParler";
+            this.buttonParler.Size = new System.Drawing.Size(159, 56);
+            this.buttonParler.TabIndex = 2;
+            this.buttonParler.Text = "Parler";
+            this.buttonParler.UseVisualStyleBackColor = true;
+            this.buttonParler.Click += new System.EventHandler(this.buttonParler_Click);
+            // 
+            // labelImageAssociee
+            // 
+            this.labelImageAssociee.AutoSize = true;
+            this.labelImageAssociee.Location = new System.Drawing.Point(999, 40);
+            this.labelImageAssociee.Name = "labelImageAssociee";
+            this.labelImageAssociee.Size = new System.Drawing.Size(162, 25);
+            this.labelImageAssociee.TabIndex = 3;
+            this.labelImageAssociee.Text = "Image associée";
+            // 
+            // pictureBoxAssociee
+            // 
+            this.pictureBoxAssociee.Location = new System.Drawing.Point(901, 98);
+            this.pictureBoxAssociee.Name = "pictureBoxAssociee";
+            this.pictureBoxAssociee.Size = new System.Drawing.Size(361, 446);
+            this.pictureBoxAssociee.TabIndex = 4;
+            this.pictureBoxAssociee.TabStop = false;
+            // 
+            // buttonQuitter
+            // 
+            this.buttonQuitter.Location = new System.Drawing.Point(1004, 596);
+            this.buttonQuitter.Name = "buttonQuitter";
+            this.buttonQuitter.Size = new System.Drawing.Size(159, 56);
+            this.buttonQuitter.TabIndex = 5;
+            this.buttonQuitter.Text = "Quitter";
+            this.buttonQuitter.UseVisualStyleBackColor = true;
+            this.buttonQuitter.Click += new System.EventHandler(this.buttonQuitter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 702);
+            this.Controls.Add(this.buttonQuitter);
+            this.Controls.Add(this.pictureBoxAssociee);
+            this.Controls.Add(this.labelImageAssociee);
+            this.Controls.Add(this.buttonParler);
+            this.Controls.Add(this.labelMotReconnu);
+            this.Controls.Add(this.richTextBoxReco);
+            this.Name = "Form1";
+            this.Text = "Reconnaissance Vocale 1 mot";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssociee)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxReco;
+        private System.Windows.Forms.Label labelMotReconnu;
+        private System.Windows.Forms.Button buttonParler;
+        private System.Windows.Forms.Label labelImageAssociee;
+        private System.Windows.Forms.PictureBox pictureBoxAssociee;
+        private System.Windows.Forms.Button buttonQuitter;
+    }
+}
+
---
+++ SyntheseVocale/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+
+namespace SyntheseVocale
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine moteurReconnaissance;
+        private Choices formeChoisie;
+        private GrammarBuilder contraintesReconnaissance;
+        private Grammar motsAReconnaitre;
+
+        private Bitmap imageAssociee;
+        private int moteurReconnaissance_SpeechRecognized;
+        private int moteurReconnaissance_SpeechRecognitionRejected;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // instanciation d'une reconnaissance vocales
+            moteurReconnaissance = new SpeechRecognitionEngine();
+            // On précise que l'acquisition se fera sur le canal d'entrée audio par défaut (micro)
+            moteurReconnaissance.SetInputToDefaultAudioDevice();
+            // On constuir le dictionnaire des mots à reconnaissance, ceux qui ne figurent pas dans cette liste ne seront pas reconnus
+            formeChoisie = new Choices(new string[] { "carré", "cercle", "triangle" });
+            // On implante le dictionnaire dans le moteur de reconnaissance en utilisant un GrammarBuilder
+            contraintesReconnaissance = new GrammarBuilder(formeChoisie);
+            motsAReconnaitre = new Grammar(contraintesReconnaissance);
+            moteurReconnaissance.LoadGrammarAsync(motsAReconnaitre);
+
+            // Abonnement aux évènements liés à la reconnaissance vocale
+            // Evènement déclencher lorsqu'un mot est reconnu
+            moteurReconnaissance.SpeechRecognized += MoteurReconnaissance_SpeechRecognized2;
+            // Evenement déclencheur lorqu'un mot n'est pas reconnu
+            moteurReconnaissance.SpeechRecognitionRejected += MoteurReconnaissance_SpeechRecognitionRejected1;
+
+           
+
+
+        }
+
+        private void MoteurReconnaissance_SpeechRecognitionRejected1(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MoteurReconnaissance_SpeechRecognized2(object sender, SpeechRecognizedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void MoteurReconnaissance_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            // Si le mot n'est pas reconnu on informe l'utilisateur 
+            MessageBox.Show("Mot non reconnu");
+        }
+
+        private void MoteurReconnaissance_SpeechRecognized(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            // Si le mot est reconnu on affiche l'image correspond à la forme reconnue
+            // et on ajoute le texte dans le richtext box prévu à cet effet
+            richTextBoxReco.AppendText(e.Result.Text);
+            richTextBoxReco.AppendText("\n");
+            FileStream fluxImage;
+
+            switch (e.Result.Text)
+            {
+                case "carré":
+                    fluxImage = new FileStream(@"..\images\carre.jpg", FileMode.Open);
+                    pictureBoxAssociee.Image = Image.FromStream(fluxImage);
+                    fluxImage.Close();
+                    break;
+                case "cercle":
+                    fluxImage = new FileStream(@"..\images\cercles.jpg", FileMode.Open);
+                    pictureBoxAssociee.Image = Image.FromStream(fluxImage);
+                    fluxImage.Close();
+                    break;
+                case "triangle":
+                    fluxImage = new FileStream(@"\..\images\triangles.jpg", FileMode.Open);
+                    pictureBoxAssociee.Image = Image.FromStream(fluxImage);
+                    fluxImage.Close();
+                    break;
+                default: break;
+
+            }
+        }
+
+        private void buttonParler_Click(object sender, EventArgs e)
+        {
+            // on ne reconnait qu'un mot à la fois
+            moteurReconnaissance.RecognizeAsync(RecognizeMode.Single);
+        }
+
+        private void buttonQuitter_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
